f08768551fc295bf6a89ba487b1d334af9d42c99
Khin-KHTO <khin.hto79@gmail.com>
2024-12-19 02:34:59 +0100

    newlink

---
+++ login.js
@@ -0,0 +1,12 @@
+function greet() {
+  console.log("Hello World!");
+}
+
+function mainFeature() {
+  return "This is the main feature";
+}
+
+//Run main features
+
+greet();
+console.log(mainFeature());
